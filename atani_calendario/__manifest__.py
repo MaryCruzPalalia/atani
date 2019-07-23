bc6b133fd709b7d13e9e7f0f74b1f85a05fd7d5e
MaryCruzPalalia <mpm@soluciones4g.com>
2019-07-23 17:31:41 -0500

    [ADD] Mostrar el tipo de marcar y modelo para los productos

---
+++ atani_calendario/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "calendario add field",
+    'description': """
+        Muestra el cmapo de calendario ubucacion pero de tipo url
+    """,
+    'author': "Soluciones4G",
+    'website': "http://www.soluciones4g.com",
+    'version': '0.1',
+    'depends': [
+                'base',
+                'calendar',
+                ],
+    'data': [
+       
+        'views/form_calendar.xml',
+              
+
+        ],
+    'installable':True,
+    'auto_install':False,
+}
---
+++ atani_calendario/models/calendar.py
@@ -0,0 +1,12 @@
+ #- * - coding: utf-8 - * -
+
+from odoo import api
+from odoo import fields
+from odoo import models
+
+class purchase_stock_recepcion(models.Model):
+    _inherit = 'calendar.event'
+
+
+   # x_xmodelo = fields.Char(string='Modelo')
+   
